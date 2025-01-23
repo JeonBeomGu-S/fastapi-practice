af194bd35b629293c12efb7946cb0d6ac5e99dd7
Beomgu <jb98967@gmail.com>
2025-01-23 16:47:33 -0500

    practice to use controller(router)

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-
+from controller import items
+from controller import users
 app = FastAPI()
 
+app.include_router(items.router)
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
